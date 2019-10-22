a0701152192c9ab7f52a06a61446a5d2632c93a6
钟加仁 <zhongjiaren@shimo.im>
2019-10-22 16:11:35 +0800

    lint

--- examples/src/root.tsx
+++ examples/src/root.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, Platform } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import { NavigationInjectedProps } from 'react-navigation';
 
 import styles from './styles';
