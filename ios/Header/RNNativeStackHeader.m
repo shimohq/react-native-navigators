4165e3fd3ce75fb8352c51856189b7a8d7ac88fa
钟加仁 <zhongjiaren@shimo.im>
2019-10-23 11:38:30 +0800

    perf: optimize updating header

--- ios/Header/RNNativeStackHeader.m
+++ ios/Header/RNNativeStackHeader.m
@@ -7,62 +7,73 @@
 {
     __weak UINavigationBar *_navigationBar;
     __weak UINavigationItem *_navigationItem;
+    UIImage *_backgroundImage;
+    UIBarButtonItem *_leftBarButtonItem;
+    UIBarButtonItem *_rightBarButtonItem;
     UIView *_bottomBorderRender;
 }
-
 
 - (void)didUpdateReactSubviews
 {
     [self updateNavigationItem];
 }
 
-
 - (void)setHeaderBackgroundColor:(UIColor *)headerBackgroundColor
 {
-    if (_navigationBar) {
-        if (headerBackgroundColor) {
-            if (CGColorGetAlpha(headerBackgroundColor.CGColor) == 0.) {
-                [_navigationBar setBackgroundImage:[UIImage new] forBarMetrics:UIBarMetricsDefault];
-                [_navigationBar setBarTintColor:[UIColor clearColor]];
-            } else {
-                [_navigationBar setBackgroundImage:nil forBarMetrics:UIBarMetricsDefault];
-                [_navigationBar setBarTintColor:headerBackgroundColor];
-            }
-        } else {
-            [_navigationBar setBarTintColor:nil];
-        }
-    }
-    
     _headerBackgroundColor = headerBackgroundColor;
+    if (!_navigationBar) {
+        return;
+    }
+    if (headerBackgroundColor) {
+        if (CGColorGetAlpha(headerBackgroundColor.CGColor) == 0.) {
+            if (!_backgroundImage) {
+                _backgroundImage = [UIImage new];
+            }
+            [_navigationBar setBackgroundImage:_backgroundImage forBarMetrics:UIBarMetricsDefault];
+            [_navigationBar setBarTintColor:[UIColor clearColor]];
+        } else {
+            [_navigationBar setBackgroundImage:nil forBarMetrics:UIBarMetricsDefault];
+            [_navigationBar setBarTintColor:headerBackgroundColor];
+        }
+    } else {
+        [_navigationBar setBackgroundImage:nil forBarMetrics:UIBarMetricsDefault];
+        [_navigationBar setBarTintColor:nil];
+    }
 }
 
 - (void)setHeaderBorderColor:(UIColor *)headerBorderColor
 {
-    if (_navigationBar) {
-        if (headerBorderColor) {
-            if (!_bottomBorderRender) {
-                CGRect navigationBarFrame = _navigationBar.frame;
-                CGRect bottomBorderRect = CGRectMake(CGRectGetMinX(navigationBarFrame), CGRectGetHeight(navigationBarFrame), CGRectGetWidth(navigationBarFrame), 1.0f / [UIScreen mainScreen].scale);
-                _bottomBorderRender = [[UIView alloc] initWithFrame:bottomBorderRect];
+    _headerBorderColor = headerBorderColor;
+    if (!_navigationBar) {
+        return;
+    }
+    if (headerBorderColor) {
+        if (!_bottomBorderRender) {
+            CGRect navigationBarFrame = _navigationBar.frame;
+            CGRect bottomBorderRect = CGRectMake(CGRectGetMinX(navigationBarFrame), CGRectGetHeight(navigationBarFrame), CGRectGetWidth(navigationBarFrame), 1.0f / [UIScreen mainScreen].scale);
+            _bottomBorderRender = [[UIView alloc] initWithFrame:bottomBorderRect];
+            _bottomBorderRender.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleTopMargin;
+        }
+        if (_bottomBorderRender.superview) {
+            if (_bottomBorderRender.superview == _navigationBar) {
+                [_navigationBar bringSubviewToFront:_bottomBorderRender];
             } else {
                 [_bottomBorderRender removeFromSuperview];
+                [_navigationBar addSubview:_bottomBorderRender];
             }
-            
-            [_bottomBorderRender setBackgroundColor:headerBorderColor];
-            [_navigationBar addSubview:_bottomBorderRender];
         } else {
-            [_bottomBorderRender removeFromSuperview];
+            [_navigationBar addSubview:_bottomBorderRender];
         }
+        [_bottomBorderRender setBackgroundColor:headerBorderColor];
+    } else {
+        [_bottomBorderRender removeFromSuperview];
     }
-    
-    _headerBorderColor = headerBorderColor;
 }
 
 - (void)attachViewController:(UIViewController *)viewController
 {
     _navigationItem = viewController.navigationItem;
-    UINavigationController *navigationController = viewController.navigationController;
-    _navigationBar = navigationController.navigationBar;
+    _navigationBar = viewController.navigationController.navigationBar;
     
     [self setHeaderBackgroundColor:_headerBackgroundColor];
     [self setHeaderBorderColor:_headerBorderColor];
@@ -74,33 +85,33 @@
     if (!_navigationItem) {
         return;
     }
-    
     _navigationItem.hidesBackButton = YES;
-    _navigationItem.titleView = nil;
-    _navigationItem.leftBarButtonItem = nil;
-    _navigationItem.rightBarButtonItem = nil;
-
+    
+    UIView *titleView = nil;
+    UIBarButtonItem *leftBarButtonItem = nil;
+    UIBarButtonItem *rightBarButtonItem = nil;
     for (RNNativeStackHeaderItem *headerItem in self.reactSubviews) {
         switch (headerItem.type) {
             case RNNativeStackHeaderTypeCenter:
-            {
-                _navigationItem.titleView = headerItem;
-            }
+                titleView = headerItem;
                 break;
             case RNNativeStackHeaderTypeLeft:
-            {
-                UIBarButtonItem *buttonItem = [[UIBarButtonItem alloc] initWithCustomView:headerItem];
-                _navigationItem.leftBarButtonItem = buttonItem;
-            }
+                if (!_leftBarButtonItem || _leftBarButtonItem.customView != headerItem) {
+                    _leftBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:headerItem];
+                }
+                leftBarButtonItem = _leftBarButtonItem;
                 break;
             case RNNativeStackHeaderTypeRight:
-            {
-                UIBarButtonItem *buttonItem = [[UIBarButtonItem alloc] initWithCustomView:headerItem];
-                _navigationItem.rightBarButtonItem = buttonItem;
-            }
+                if (!_rightBarButtonItem || _rightBarButtonItem.customView != headerItem) {
+                    _rightBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:headerItem];
+                }
+                rightBarButtonItem = _rightBarButtonItem;
                 break;
         }
     }
+    _navigationItem.titleView = titleView;
+    _navigationItem.leftBarButtonItem = leftBarButtonItem;
+    _navigationItem.rightBarButtonItem = rightBarButtonItem;
 }
 
 - (void)detachViewController
@@ -119,8 +130,8 @@
 {
     if (_bottomBorderRender) {
         [_bottomBorderRender removeFromSuperview];
-        _bottomBorderRender = nil;
     }
+    [_navigationBar setBackgroundImage:nil forBarMetrics:UIBarMetricsDefault];
     [_navigationBar setBarTintColor:nil];
 }
 
