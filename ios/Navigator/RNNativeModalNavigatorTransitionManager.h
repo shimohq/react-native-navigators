9956d049589ecb35aa295bf8da3b3cbb750d934d
钟加仁 <zhongjiaren@shimo.im>
2019-10-29 14:18:26 +0800

    add RNNativeModalNavigatorTransitionManager.

---
+++ ios/Navigator/RNNativeModalNavigatorTransitionManager.h
@@ -0,0 +1,31 @@
+//
+//  RNNativeModalNavigatorTransitionManager.h
+//  react-native-navigators
+//
+//  Created by Bell Zhong on 2019/10/29.
+//
+
+#import <Foundation/Foundation.h>
+#import "RNNativeStackConst.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RNNativeModalNavigatorTransitionManager : NSObject
+
+@property (nonatomic, copy) RNNativeNavigatorTransitionBlock endTransition;
+
+- (void)decreaseAndHandleEndTransition;
+- (NSInteger)decrease;
+- (NSInteger)increment;
+
+- (void)presentViewController:(UIViewController *)viewController
+         parentViewController:(UIViewController *)parentViewController
+                     animated:(BOOL)animated
+                   completion:(void (^ __nullable)(void))completion;
+- (void)dismissViewController:(UIViewController *)viewController
+                     animated:(BOOL)animated
+                   completion:(void (^ __nullable)(void))completion;
+
+@end
+
+NS_ASSUME_NONNULL_END
