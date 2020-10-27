e2073aed2852b929d42798cea50a5ef035d95311
谢天宸 <xietianchen@shimo.im>
2020-10-27 14:40:59 +0800

    chore: 调整 scene和项目重名

--- ios/RNNativeSceneController.h
+++ ios/RNNativeSceneController.h
@@ -7,18 +7,18 @@
 
 @interface RNNativeSceneController : UIViewController
 
-@property (nonatomic, weak) RNNativeScene *scene;
-@property (nonatomic, assign) RNNativeSceneStatus status;
+@property(nonatomic, weak) RNNativeScene *rnScene;
+@property(nonatomic, assign) RNNativeSceneStatus status;
 
 /**
  status bar 样式
  */
-@property (nonatomic, assign) UIStatusBarStyle statusBarStyle;
+@property(nonatomic, assign) UIStatusBarStyle statusBarStyle;
 
 /**
  是否隐藏 status bar
  */
-@property (nonatomic, assign) BOOL statusBarHidden;
+@property(nonatomic, assign) BOOL statusBarHidden;
 
 - (instancetype)initWithScene:(RNNativeScene *)scene;
 
