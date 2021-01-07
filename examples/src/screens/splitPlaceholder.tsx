ba4d1dfbec68025d8b9f9e9a32062b3eda0418cc
陈阳 <chenyang@shimo.im>
2021-01-07 15:03:03 +0800

    Add navigate in placeholder example

--- examples/src/screens/splitPlaceholder.tsx
+++ examples/src/screens/splitPlaceholder.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, TouchableOpacity } from 'react-native';
+import { NavigationInjectedProps } from 'react-navigation';
 
-export default function SplitPlaceholder() {
+export default function SplitPlaceholder(props: NavigationInjectedProps) {
   return (
     <View style={{ flex: 1, borderColor: 'orange', borderWidth: 2, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Primary Placeholder</Text>
+      <TouchableOpacity onPress={() => props.navigation.navigate('secondary')}><Text>Go to secondary scene</Text></TouchableOpacity>
     </View>
   );
 }
