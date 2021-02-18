0fd808b3c19fe99154a18d6c6b56b23b4d22bf11
陈阳 <chenyang@shimo.im>
2021-02-18 10:12:47 +0800

    Add export for NativeNavigationClosingStateContext

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from './createNavigators';
 export { default as SplitRouter } from './SplitRouter';
+export { NativeNavigationClosingStateContext } from './NativeScene';
 export * from './splitNavigatorRules';
 export * from './types';
