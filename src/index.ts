710f94b154e60a0b8ed77c856a4805bb45e1f82b
Bell <bell@greedlab.com>
2021-03-11 18:02:47 +0800

    feature: add splitLineColor support (#53)
    Co-authored-by: 钟加仁 <zhongjiaren@shimo.im>

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 export * from './createNavigators';
 export { default as SplitRouter } from './SplitRouter';
 export { NativeNavigationClosingStateContext } from './NativeScene';
-export { NativeSplitNavigatorOptionsContext } from './NativeSplitNavigatorOptionsWrapper';
+export {
+  NativeSplitNavigatorOptionsContext
+} from './NativeSplitNavigatorOptionsWrapper';
 export * from './splitNavigatorRules';
 export * from './types';
