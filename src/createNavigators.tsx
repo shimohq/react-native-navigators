c36671378af19528bb2f43376e75f4c4a7d6f055
陈阳 <chenyang@shimo.im>
2021-04-14 12:59:53 +0800

    Remove preventNavigatorUpdateWhileClosing

--- src/createNavigators.tsx
+++ src/createNavigators.tsx
@@ -1,11 +1,8 @@
-import React, { useContext, ComponentType } from 'react';
 import {
   createNavigator,
   StackRouter,
   NavigationRouteConfigMap
 } from 'react-navigation';
-import StaticContainer from 'static-container';
-import hoistNonReactStatics from 'hoist-non-react-statics';
 
 import NativeNavigators from './NativeNavigators';
 import SplitRouter from './SplitRouter';
@@ -15,38 +12,13 @@ import {
   NativeNavigationOptions,
   NativeNavigatorModes
 } from './types';
-import { NativeNavigationClosingStateContext } from './NativeScene';
-
-/**
- * Prevent Navigator updating while closing transition is performed.
- *
- * @param Navigator {ComponentType}
- * @returns {ComponentType}
- */
-function preventNavigatorUpdateWhileClosing<Props>(
-  Navigator: ComponentType<Props>
-) {
-  const ClosingUpdatePreventedNavigator = (props: Props) => {
-    const closing = useContext(NativeNavigationClosingStateContext);
-
-    return (
-      <StaticContainer shouldUpdate={!closing}>
-        <Navigator {...props} />
-      </StaticContainer>
-    );
-  };
-
-  return hoistNonReactStatics(ClosingUpdatePreventedNavigator, Navigator);
-}
 
 export function createNativeNavigator(
   routeConfigMap: NavigationRouteConfigMap<NativeNavigationOptions, any>,
   stackConfig: NativeNavigationRouterConfig
 ) {
   const router = StackRouter(routeConfigMap, stackConfig);
-  return preventNavigatorUpdateWhileClosing(
-    createNavigator(NativeNavigators, router, stackConfig)
-  );
+  return createNavigator(NativeNavigators, router, stackConfig);
 }
 
 export function createStackNavigator(
@@ -54,12 +26,10 @@ export function createStackNavigator(
   stackConfig: Omit<NativeNavigationRouterConfig, 'mode'>
 ) {
   const router = StackRouter(routeConfigMap, stackConfig);
-  return preventNavigatorUpdateWhileClosing(
-    createNavigator(NativeNavigators, router, {
-      ...stackConfig,
-      mode: NativeNavigatorModes.Stack
-    })
-  );
+  return createNavigator(NativeNavigators, router, {
+    ...stackConfig,
+    mode: NativeNavigatorModes.Stack
+  });
 }
 
 export function createCardNavigator(
@@ -67,12 +37,10 @@ export function createCardNavigator(
   stackConfig: Omit<NativeNavigationRouterConfig, 'headerMode' | 'mode'>
 ) {
   const router = StackRouter(routeConfigMap, stackConfig);
-  return preventNavigatorUpdateWhileClosing(
-    createNavigator(NativeNavigators, router, {
-      ...stackConfig,
-      mode: NativeNavigatorModes.Card
-    })
-  );
+  return createNavigator(NativeNavigators, router, {
+    ...stackConfig,
+    mode: NativeNavigatorModes.Card
+  });
 }
 
 export function createSplitNavigator(
@@ -80,10 +48,8 @@ export function createSplitNavigator(
   splitConfig: Omit<NativeSplitNavigationRouterConfig, 'headerMode' | 'mode'>
 ) {
   const router = SplitRouter(routeConfigMap, splitConfig);
-  return preventNavigatorUpdateWhileClosing(
-    createNavigator(NativeNavigators, router, {
-      ...splitConfig,
-      mode: NativeNavigatorModes.Split
-    })
-  );
+  return createNavigator(NativeNavigators, router, {
+    ...splitConfig,
+    mode: NativeNavigatorModes.Split
+  });
 }
